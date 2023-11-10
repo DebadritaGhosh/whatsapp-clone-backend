8e650c5e32d27963eee1c438a1e3567aec4f21e5
Debadrita Ghosh <debadritaghosh@Debadritas-Mac-mini.local>
2023-11-10 08:16:08 +0530

    exception handling

--- src/index.js
+++ src/index.js
@@ -6,6 +6,35 @@ import logger from "./configs/logger.config.js";
 //env variables
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
+let server = app.listen(PORT, () => {
     logger.info(`Server is listening at ${PORT}`);
+    // throw new Error("Error in server");
 });
+
+const exitHandler = () => {
+    if(server){
+        logger.info("Server closed.");
+        process.exit(1);
+    }else{
+        process.exit(1);
+    }
+}
+
+//Handle server errors
+const unexpectedErrorHndler = (e) => {
+    logger.error(e);
+    exitHandler();
+}
+
+
+//Event listeners
+process.on("uncaughtException", unexpectedErrorHndler);
+process.on("unhandledRejection",unexpectedErrorHndler);
+
+//SIGTERM
+process.on("SIGTERM",() => {
+    if(server){
+        logger.info("Server closed.");
+        process.exit(1);
+    }
+})
